50ce75a66edc708600cf602185b477810940abf0
Ouzed <mhamed.ouzed@edu.uiz.ac.ma>
2024-03-30 14:44:44 +0100

    I add files and i maked the header

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Search box
+let mybox=document.querySelector("header .seorch .box")
+let myboxInp=document.querySelector("header .seorch .box input")
+document.addEventListener("click",function(event){
+    if(mybox.contains(event.target)){
+        myboxInp.style.width="100px"
+    mybox.style.border = '1px solid rgba(231, 231, 231, 0.40)';
+    mybox.style.backgroundColor = '#1E1B33';
+    }
+    else{
+        myboxInp.style.width = "0"; // Or set to your desired default width
+        mybox.style.border = 'none'; // Or set to your desired default border
+        mybox.style.backgroundColor = '#04011C'
+        myboxInp.value=""
+    }
+    
+})
+
